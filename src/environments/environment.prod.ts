521c2a7a8f0bfff71f652764ae87e8e5b9195162
VanHoanTran <89094498+VanHoanTran@users.noreply.github.com>
2022-08-05 18:50:56 -0400

    correct userAPIBase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   clientID: "a2b5e776fee0452698dcfa9b000e6542",
   clientSecret: "4fd1ae07e6144fd7b0e6e720ded52e85",
-  userAPIBase: "https:/hidden-reef-44552.herokuapp.com/api/user"
+  userAPIBase: "https://hidden-reef-44552.herokuapp.com/api/user"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   clientID: 'a2b5e776fee0452698dcfa9b000e6542',
   clientSecret: '4fd1ae07e6144fd7b0e6e720ded52e85',
-  userAPIBase: "https:/hidden-reef-44552.herokuapp.com/api/user"
+  userAPIBase: "https://hidden-reef-44552.herokuapp.com/api/user"
 };
 
 /*
